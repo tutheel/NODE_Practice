2f001f44fb58ceb14665031bd093d6ed7d619a04
Sushil <sushilpatil953825@gmail.com>
2024-08-13 00:20:38 +0530

    First Commit

---
+++ Modules/MathFun.js
@@ -0,0 +1,13 @@
+//Syntax-1
+// const add = (a, b) => a + b;
+// const subtract = (a, b) => a - b;
+// const multiply = (a, b) => a * b;
+// const division = (a, b) => a / b;
+
+// module.exports = { add, subtract, multiply, division };
+
+// Syntax-2
+exports.add = (a, b) => a + b;
+exports.subtract = (a, b) => a - b;
+exports.multiply = (a, b) => a * b;
+exports.division = (a, b) => a / b;
---
+++ Modules/Server.js
@@ -0,0 +1,7 @@
+//we are using Object destrucutre below
+const { add, subtract, multiply, division } = require("./MathFun");
+
+console.log(add(5, 3));
+console.log(subtract(5, 3));
+console.log(multiply(5, 3));
+console.log(division(5, 3));
